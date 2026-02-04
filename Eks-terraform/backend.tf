c370465a41163def51caef1d071a73656814a5f9
mahigit1710 <rajendranmaheswaran4@gmail.com>
2026-02-04 22:20:18 +0530

    test1

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "s3buckproject040226" # Replace with your actual S3 bucket name
+    bucket = "amznbucket040226" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-southeast-1"
   }
